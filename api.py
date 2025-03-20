a2cc346ad1afa0e1ea218ed20f3ebb92ce34104c
Alexey <aleksey-000@mail.ru>
2025-03-21 01:40:48 +0500

    маршрутизация

--- api.py
+++ api.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get('/')
 async def welcome() -> dict:
     return {'message': 'hello world'}
+
+@app.get('/hello')
+async def welcome_user(user: str) -> dict:
+    return {'user': f'hello {user}!'}
+
+@app.get('/product')
+async def detail_view(product_id: int) -> dict:
+    return {'product': f'Stock number {product_id}'}
